18e990f3155adcdb9a796f078981b8eeb3932350
successful-lad <blessedonwebdew@gmail.com>
2019-07-18 16:48:48 +0300

    Add React

---
+++ build/precache-manifest.db25e0a3fba67f4608bd7f610d38692a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58f178b2be4ff5e393f103ba2f7f3e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841c39c17a0bf834a877",
+    "url": "/static/css/main.3f366b2e.chunk.css"
+  },
+  {
+    "revision": "6078ceb7eb00a1a3dfc1",
+    "url": "/static/js/2.e319e905.chunk.js"
+  },
+  {
+    "revision": "841c39c17a0bf834a877",
+    "url": "/static/js/main.ab5e76f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
